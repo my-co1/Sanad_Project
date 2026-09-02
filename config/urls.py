2f325f77a81c2e25dba26e4520f488f63db4859a
Myco <mycokhaled@gmail.com>
2026-09-02 23:30:43 +0300

    Add files via upload

---
+++ config/urls.py
@@ -0,0 +1,20 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.1/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('donations.urls')),
+]
+
+# تخديم Static و Media Files في بيئة التطوير
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
